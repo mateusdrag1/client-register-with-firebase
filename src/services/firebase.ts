dd78aeec9a2d2b3be2dc629d71d843b6a5d2a4c6
Matthieu Christian <mateus1212122@hotmail.com>
2022-07-09 09:17:39 -0300

    feat: adds a firebase to project

---
+++ src/services/firebase.ts
@@ -0,0 +1,12 @@
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export const db = getFirestore(app);
